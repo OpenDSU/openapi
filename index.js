e4d8fd1744dc753ac8ef835a95c8def17414e5e2
mastaleru <mastaleru.rafael@gmail.com>
2022-11-04 19:47:59 +0200

    cloud-wallet, static-server, installation details

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require('express');
 const path = require('path');
 const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
 const app = express();
+const definitions = require("./documents.json");
 
 
 app.use(express.static("./"));
 app.use("/bootstrap", express.static("./node_modules/bootstrap"));
-app.get('/openapi/:document', function(req, res) {
+app.get('/openapi/:document', function (req, res) {
     res.sendFile(path.join(__dirname, '/openapi.html'));
 });
-app.get('/', function(req, res) {
-    res.redirect(301, '/openapi/anchoring');
+app.get('/', function (req, res) {
+    if (Object.keys(definitions).length > 0) {
+        const fallbackDefinition = Object.keys(definitions)[0];
+        return res.redirect(301, `/openapi/${fallbackDefinition}`);
+    }
+    res.status(404).sendFile(path.join(__dirname, "/404.html"))
+
 });
 
 app.use(express.static(pathToSwaggerUi))
